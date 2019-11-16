eeb58831c75dfe53cb955280332f0e1e4eb90284
Erik Tenecela <eriktenecela13@gmail.com>
2019-11-16 13:34:08 -0500

    fixed errors

--- models/ingredients.js
+++ models/ingredients.js
@@ -1,13 +1,12 @@
 module.exports = (sequelize, DataTypes) => {
-    const ingredients = sequelize.define("ingredients", {
-        name: {
-            type: DataTypes.STRING,
-            allowNull: false,
-            validate: {
-                len: [1]
-            }
-
-        }
-    });
-    return ingredients;
-}
+  const ingredients = sequelize.define("ingredients", {
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1]
+      }
+    }
+  });
+  return ingredients;
+};
